b79fef72caac411292a45a67146864e964cdeb72
Park YongJune <redplus@redplus.net>
2025-01-30 19:25:16 +0900

    .

--- BlazorApp/Startup.cs
+++ BlazorApp/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
